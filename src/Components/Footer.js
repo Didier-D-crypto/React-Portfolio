162f89d6da2c8947e3d80084d63bd30a34f9b5d4
dalex <delphindidier0@gmail.com>
2020-07-09 19:33:55 -0400

    file syntax updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,7 +20,7 @@ class Footer extends Component {
 
            <ul className="copyright">
               <li>&copy; Copyright 2020 Didier Delphin</li>
-              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Styleshout</a></li>
+              <li>Design by <a title="Styleshout" href="http://www.styleshout.com/">Didier Delphin</a></li>
            </ul>
 
         </div>
